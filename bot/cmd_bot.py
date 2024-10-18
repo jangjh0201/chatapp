39f24aa77ad2f847a03e670c04d12d0ca8af9151
jangjh0201 <jangjh0201@gmail.com>
2024-10-18 18:25:24 +0900

    Mod : 변수명 수정

--- bot/cmd_bot.py
+++ bot/cmd_bot.py
@@ -3,8 +3,8 @@ from module.cmd.konlpy_lib import Konlpy
 
 class CMDBot:
     def __init__(self):
-        self.cmd_bot = Konlpy()
+        self.cmd_module = Konlpy()
 
     def get_command(self, sentence):
-        nouns = self.cmd_bot.get_nouns(sentence)
+        nouns = self.cmd_module.get_nouns(sentence)
         return nouns
