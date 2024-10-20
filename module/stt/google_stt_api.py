dba02e297166de1c06e8498d818dfbf761e8923a
jangjh0201 <jangjh0201@gmail.com>
2024-10-21 01:53:55 +0900

    Mod : 동적 경로 할당

--- module/stt/google_stt_api.py
+++ module/stt/google_stt_api.py
@@ -7,7 +7,7 @@ import scipy.io.wavfile as wavfile
 
 
 class GoogleSpeechToText:
-    def __init__(self, file_path="resource/audio/stt/temp_audio.wav"):
+    def __init__(self):
         # .env 파일 로드
         load_dotenv()
         # Google API 키 경로 설정
@@ -16,9 +16,8 @@ class GoogleSpeechToText:
         )
         # Google Cloud Speech-to-Text 클라이언트 설정
         self.client = speech.SpeechClient()
-        self.file_path = file_path
 
-    def record(self, seconds):
+    def record(self, file_path, seconds):
         print(f"Recording {seconds} seconds of audio...")
         # 오디오 녹음 (마이크 테스트를 위해 sounddevice 사용)
         recording = sd.rec(
@@ -27,11 +26,11 @@ class GoogleSpeechToText:
         sd.wait()  # 녹음이 끝날 때까지 대기
 
         # 녹음된 데이터를 wav 파일로 저장
-        wavfile.write(self.file_path, 44100, recording)
+        wavfile.write(file_path, 44100, recording)
 
         try:
             # Google Cloud Speech-to-Text API 호출을 위해 오디오 파일 읽기
-            with open(self.file_path, "rb") as audio_file:
+            with open(file_path, "rb") as audio_file:
                 content = audio_file.read()
                 audio = speech.RecognitionAudio(content=content)
 
