cf7caaac4130f6d2fffcc136f1f026b97849653e
jangjh0201 <jangjh0201@gmail.com>
2024-10-21 16:26:51 +0900

    Mod : 실행 흐름 수정
    ref : cfea5c31429a1132e1c73bfeb8bc5bbd59c18615,
    fc9a30a9c9a050408f4acb3173f78e28a87d1f3c

--- app/chatapp.py
+++ app/chatapp.py
@@ -1,11 +1,12 @@
-from bot import cmd_bot, stt_bot, tts_bot
+from bot import ai_bot, cmd_bot, stt_bot, tts_bot
 
 
 class ChatApp:
     def __init__(self):
         self.stt_bot = stt_bot.STTBot()
         self.cmd_bot = cmd_bot.CMDBot()
-        self.tts_bot = tts_bot.TTSBot()
+        self.ai_bot = ai_bot.AIBot()
+        self.tts_bot = tts_bot.TTSBot(google_api=True)
 
     def create_response(self, command_list):
         response = ""
@@ -27,13 +28,20 @@ class ChatApp:
         sentence = self.stt_bot.listen()
         print(f"음성 입력: {sentence}")
 
-        # 명령어 추출 (CMD)
-        command_list = self.cmd_bot.get_command(sentence)
-        print(f"명령어 리스트: {command_list}")
+        # # 명령어 추출 (CMD)
+        # command_list = self.cmd_bot.get_command(sentence)
+        # print(f"명령어 리스트: {command_list}")
 
-        # 명령어에 따른 응답 생성
-        response = self.create_response(command_list)
+        # # 명령어에 따른 응답 생성
+        # response = self.create_response(command_list)
+
+        # AI 모델을 사용하여 응답 생성
+        response = self.ai_bot.generate_response(sentence)
+
+        # 응답 파싱
+        parsed_response, parsed_command = cmd_bot.CMDBot().parse_sentence(response)
 
         # 응답 메시지를 음성으로 출력 (TTS)
-        response_message = self.tts_bot.speak(response)
-        print(f"응답 메시지: {response_message}")
+        self.tts_bot.speak(parsed_response)
+        print(f"봇: {parsed_response}")
+        print(f"{parsed_command}\n")
