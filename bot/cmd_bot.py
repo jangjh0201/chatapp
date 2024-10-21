fc9a30a9c9a050408f4acb3173f78e28a87d1f3c
jangjh0201 <jangjh0201@gmail.com>
2024-10-21 16:26:12 +0900

    Add : 대괄호 파서 구현

--- bot/cmd_bot.py
+++ bot/cmd_bot.py
@@ -1,10 +1,18 @@
 from module.cmd.konlpy_lib import Konlpy
+from module.cmd.parser import Parser
 
 
 class CMDBot:
     def __init__(self):
-        self.cmd_module = Konlpy()
+        self.cmd_commander = Konlpy()
+        self.cmd_parser = Parser()
 
     def get_command(self, sentence):
-        nouns = self.cmd_module.get_nouns(sentence)
+        nouns = self.cmd_commander.get_nouns(sentence)
         return nouns
+
+    def parse_sentence(self, sentence):
+        spoken_part, command_list = self.cmd_parser.parse_sentence_with_commands(
+            sentence
+        )
+        return spoken_part, command_list
