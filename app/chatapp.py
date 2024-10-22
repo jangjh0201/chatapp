f0355bbbc7f2f0a18620f942ad32a4ed408c27b3
jangjh0201 <jangjh0201@gmail.com>
2024-10-22 13:48:04 +0900

    Mod : cmd bot 수정, 오류 메시지 수정, 주석 달기

--- app/chatapp.py
+++ app/chatapp.py
@@ -2,38 +2,61 @@ from bot import ai_bot, cmd_bot, stt_bot, tts_bot
 
 
 class ChatApp:
+    """
+    음성 대화 시나리오를 수행하는 ChatApp 클래스.
+    음성 입력을 받아들여 명령어를 추출하고, 해당 명령어에 따른 응답을 생성합니다.
+    """
+
     def __init__(self):
+        """
+        ChatApp 클래스 초기화. STTBot, CMDBot, AIBot, TTSBot을 초기화합니다.
+        생성된 봇들을 사용하여 음성 대화 시나리오를 수행합니다.
+        """
         self.stt_bot = stt_bot.STTBot()
         self.cmd_bot = cmd_bot.CMDBot()
         self.ai_bot = ai_bot.AIBot()
         self.tts_bot = tts_bot.TTSBot(google_api=True)
 
-    def create_response(self, command_list):
-        response = ""
-        if "커피" in command_list or "아메리카노" in command_list:
-            response = "커피를 만들어 드릴게요."
-        elif "차" in command_list or "캐모마일" in command_list:
-            response = "캐모마일 차를 만들어 드릴게요."
-        elif "아이스티" in command_list:
-            response = "아이스티를 만들어 드릴게요."
-        elif "출근" in command_list:
-            response = "출근 준비를 도와드릴게요."
-        else:
-            response = "이해하지 못했어요. 다시 말씀해 주세요."
+    def run(self, mode="ai"):
+        """
+        ChatApp을 실행하는 메소드.
+        음성 입력을 받아들여 명령어를 추출하고, 해당 명령어에 따른 응답을 생성합니다.
+        Args:
+            mode (str): 실행 모드 (default: "ai")
+        """
+        if mode == "normal":
+            self.normal_mode()
+        elif mode == "ai":
+            self.ai_mode()
 
-        return response
-
-    def run(self):
+    def normal_mode(self):
+        """
+        일반 모드로 ChatApp을 실행하는 메소드.
+        음성 입력을 받아들여 명령어를 추출하고, 해당 명령어에 따른 응답을 생성합니다.
+        """
         # 음성 입력받기 (STT)
         sentence = self.stt_bot.listen()
         print(f"음성 입력: {sentence}")
 
-        # # 명령어 추출 (CMD)
-        # command_list = self.cmd_bot.get_command(sentence)
-        # print(f"명령어 리스트: {command_list}")
+        # 명령어 추출 (CMD)
+        command_list = self.cmd_bot.get_command(sentence)
+        print(f"명령어 리스트: {command_list}")
 
-        # # 명령어에 따른 응답 생성
-        # response = self.create_response(command_list)
+        # 명령어에 따른 응답 생성
+        response = self.cmd_bot.create_response(command_list)
+
+        # 응답 메시지를 음성으로 출력 (TTS)
+        self.tts_bot.speak(response)
+        print(f"봇: {response}")
+
+    def ai_mode(self):
+        """
+        AI 모드로 ChatApp을 실행하는 메소드.
+        음성 입력을 받아들여 AI 모델을 사용하여 응답을 생성합니다.
+        """
+        # 음성 입력받기 (STT)
+        sentence = self.stt_bot.listen()
+        print(f"음성 입력: {sentence}")
 
         # AI 모델을 사용하여 응답 생성
         response = self.ai_bot.generate_response(sentence)
